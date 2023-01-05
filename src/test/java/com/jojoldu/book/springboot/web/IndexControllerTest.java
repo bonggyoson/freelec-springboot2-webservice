13ac6b580f14e6deeba269679f794ed4cbe566f0
bonggyoson <bonggyoson@gmail.com>
2023-01-05 22:32:45 +0900

    feat : Google Login, Naver Login

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
@@ -19,10 +19,10 @@ public class IndexControllerTest {
 
     @Test
     public void 메인페이지_로딩() {
-        // when
+        //when
         String body = this.restTemplate.getForObject("/", String.class);
 
-        // then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
